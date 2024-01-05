7e86ebb8180180da6bd766b4c44937a470a87ca7
Kaburi <specialemail90@gmail.com>
2024-01-05 10:35:33 +0900

    테스트 제거

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,6 @@
 import ProductGroupList from "@/components/(main-pages)/root/product-group-list";
-import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default async function MainPage() {
-  return (
-    <>
-      <Button>gkdl</Button>
-      <Button>gkdl</Button>
-      <Button>gkdl</Button>
-      <Button>gkdl</Button>
-      <ProductGroupList />
-    </>
-  );
+  return <ProductGroupList />;
 }
