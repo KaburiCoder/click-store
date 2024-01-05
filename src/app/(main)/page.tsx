88c17f43cb3f12748bb1df68e18f4ff1f7520ef2
Kaburi <specialemail90@gmail.com>
2024-01-05 10:21:36 +0900

    버튼 테스트 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,14 @@
 import ProductGroupList from "@/components/(main-pages)/root/product-group-list";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default async function MainPage() {
   return (
     <>
+      <Button>gkdl</Button>
+      <Button>gkdl</Button>
+      <Button>gkdl</Button>
+      <Button>gkdl</Button>
       <ProductGroupList />
     </>
   );
