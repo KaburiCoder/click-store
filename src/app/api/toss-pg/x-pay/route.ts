e9c118a68c97e21825c68ed070928ee24ee5545a
Kaburi <specialemail90@gmail.com>
2024-01-09 09:40:15 +0900

    api/toss-pg/x-pay route 추가

---
+++ src/app/api/toss-pg/x-pay/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    console.log("get - ", await req.json());
+
+    return NextResponse.json({ message: "success" });
+  } catch (error: any) {
+    return NextResponse.json({ errorMessage: error.message });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    console.log("post - ", await req.json());
+
+    return NextResponse.json({ message: "success" });
+  } catch (error: any) {
+    return NextResponse.json({ errorMessage: error.message });
+  }
+}
