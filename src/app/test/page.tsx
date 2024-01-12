3213f647a65648c0002502a9dbbe88fe9c728775
Kaburi <specialemail90@gmail.com>
2024-01-12 10:44:45 +0900

    .

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 import ScreenLoading from "@/components/(shared)/screen-loading";
-import React from "react";
+import React, { useState } from "react";
 
 export default function TestPage() {
+  const [error, setError] = useState(false);
   console.log("console.log");
   console.warn("console.warn");
   console.error("console.error");
   console.table({ a: "123", b: "456" });
+
+  if (error) throw new Error("강제 에러 발생 ㅠㅠ");
+
   return (
-    <div className="bg-red-500 h-full w-full">
-      <ScreenLoading />
+    <div className="h-full w-full bg-red-500">
+      <button
+        onClick={() => {
+          setError(true);
+        }}
+      >
+        강제 에러 발생 테스트
+      </button>
     </div>
   );
 }
