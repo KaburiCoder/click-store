238828229307907803bf49a1e1fdf46b0dc75d0d
Kaburi <specialemail90@gmail.com>
2024-07-20 17:39:45 +0900

    paymentbody 분리

---
+++ src/app/(main)/payment/payment-body.tsx
@@ -0,0 +1,76 @@
+import WrongApproach from "@/components/(errors)/wrong-approach";
+import { Card } from "@/components/ui/card";
+import ButtonL from "@/components/ui/custom/button-l";
+import { APP_ENV } from "@/configs/config";
+import { useCartView } from "@/lib/hooks/use-cart-view";
+import useTossWidget from "@/lib/hooks/use-toss-widget";
+import usePaymentStore from "@/store/payment.store";
+import React, { useEffect } from "react";
+
+const PaymentBody = () => {
+  const {
+    orderRequestMessage,
+    hasNotPaymentData,
+    cartItemsUtil,
+    getPaymentItems,
+  } = useCartView({
+    isPayment: true,
+  });
+  const { setPaymentState, clear } = usePaymentStore();
+  const { fetchCheckout } = useTossWidget({
+    orderName: cartItemsUtil?.orderName!,
+    totalPrice: cartItemsUtil?.totalPrice!,
+  });
+
+  if (hasNotPaymentData) {
+    return <WrongApproach />;
+  }
+
+  function checkoutHandler() {
+    fetchCheckout({
+      onPreCall: () => {
+        setPaymentState({
+          orderName: cartItemsUtil!.orderName,
+          quantity: cartItemsUtil!.totalQuantity,
+          paymentItems: getPaymentItems(),
+          cartItemIds: cartItemsUtil!.cartItemIds,
+          orderRequestMessage,
+        });
+      },
+      onFail: clear,
+    });
+  }
+
+  useEffect(() => {
+    console.error(
+      `process.env.NEXT_PUBLIC_APP_ENV: ${process.env.NEXT_PUBLIC_APP_ENV}`,
+    );
+  }, []);
+  return (
+    <div className="max-w-6x mx-auto">
+      <Card className="m-2">
+        <div id="payment-widget" />
+        <div id="agreement" />
+      </Card>
+      <Card className="flex-center mx-2 flex-col px-2 py-4">
+        <div className="mb-2 text-black">
+          결제금액{" : "}
+          <span className="text-xl font-bold text-blue-900">
+            {cartItemsUtil?.totalPrice?.toLocaleString()}원
+          </span>
+        </div>
+
+        <ButtonL className="w-full text-base" onClick={checkoutHandler}>
+          결제하기
+        </ButtonL>
+        {APP_ENV !== "prod" && (
+          <div className="mt-2 w-full rounded bg-rose-200 p-3 text-center text-red-600">
+            테스트 환경입니다. 실제 결제가 되지 않습니다.
+          </div>
+        )}
+      </Card>
+    </div>
+  );
+};
+
+export default PaymentBody;
